cb44ea5450c642c2d9138d2f611285bbbb563975
RomakinNum1 <stud115405@vyatsu.ru>
2021-10-24 18:57:09 +0300

    Доработан Makefile

--- web/public/dataBaseEditor.php
+++ web/public/dataBaseEditor.php
@@ -148,38 +148,11 @@ class dataBaseEditor
         return $res;
     }
 
-    static function SelectForToken($connect, $data)
-    {
-        $decoded = JWT::decode($data, $_ENV['JWT_KEY'], array('HS256'));
-        $checkLogin = $connect->prepare('SELECT * FROM users WHERE id = :id');
-        $checkLogin->execute(['id' => $decoded->id]);
-
-        return $checkLogin->fetch(PDO::FETCH_ASSOC);
-    }
-
     static function SelectLogin($connect, $data, $sql)
     {
         $checkLogin = $connect->prepare($sql);
         $checkLogin->execute(['login' => $data['login']]);
 
         return $checkLogin->fetch(PDO::FETCH_ASSOC);
-    }
-
-    static function addMessage($dataBaseConnect, $data)
-    {
-        $resultDB = $dataBaseConnect->prepare("insert into messages values (null, :name, :message)");
-        $resultDB->execute($data);
-    }
-
-    static function getMessage($dataBaseConnect): array
-    {
-        $resultDB = $dataBaseConnect->prepare("SELECT * FROM messages ");
-        $resultDB->execute();
-
-        $output = [];
-        while ($row = $resultDB->fetch(PDO::FETCH_ASSOC)) {
-            $output[] = $row;
-        }
-        return $output;
     }
 }
